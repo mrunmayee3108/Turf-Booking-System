f11bdbae82cd18402adc0fef747f8c0d0cd58ec1
SiddhiThakur <53.siddhithakur@gmail.com>
2025-07-24 10:23:20 +0530

    Login Page

---
+++ login.js
@@ -0,0 +1,16 @@
+let btn=document.querySelector("button");
+let form=document.querySelector("#myform");
+btn.onclick=(e)=>{
+    e.preventDefault();
+    if(form.checkValidity()){
+        alert("Booking Successful!!");
+        form.reset();
+    }
+    else form.reportValidity();
+
+}
+// event.preventDefault() prevents the form from submitting and refreshing the page.
+
+// form.checkValidity() checks if all required fields are filled correctly.
+
+// form.reportValidity() shows built-in browser validation messages for missing or invalid inputs.
